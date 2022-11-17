bcfaf49ba0b755a4301fc1b5da89313cb16e6dc9
Yadong Zhang <zhyd007@gmail.com>
2022-11-17 11:59:48 +0800

    Feat/rainbowkit (#1)
    * feat: setup rainbowkit.
    
    * chore: removed unused api folder.
    
    * fix: added infura api key in enviroment.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,46 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { infuraProvider } from 'wagmi/providers/infura';
+import { publicProvider } from 'wagmi/providers/public';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY || '' }),
+      publicProvider()
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'Vesting Kwenta',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
