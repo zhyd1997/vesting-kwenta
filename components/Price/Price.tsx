43b841afb125ec2149d97f51950e43819152f60f
Yadong Zhang <zhyd007@gmail.com>
2022-11-17 06:31:27 +0000

    feat: integrated with Uniswap subgraph.

--- components/Price/Price.tsx
+++ components/Price/Price.tsx
@@ -1,9 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { getKwentaPrice } from "../../utils/getKwentaPrice";
 import { Container } from "../Container";
 
 type PriceProps = {};
 
 export const Price: FC<PriceProps> = () => {
+  const [price, setPrice] = useState('---');
+
+  useEffect(() => {
+    (async () => {
+      const res = await getKwentaPrice();
+
+      console.log(res);
+    })()
+  }, []);
+
   return (
     <Container>
       <p className="heading">Price</p>
